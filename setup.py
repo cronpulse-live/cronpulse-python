ad85ff09fea8979ea39b95948730a322c796fae9
Amr Sobhy <drkonafa@gmail.com>
2024-08-01 17:54:02 +0200

    update REadme

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name="cronpulse",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Amr Sobhy",
+    author_email="team@cronpulse.live",
     description="A simple monitor and wrap function for cron jobs",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/cronpulse",
+    url="https://github.com/cronpulse-live/cronpulse-python.git",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
